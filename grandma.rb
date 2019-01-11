d1dccb9f24a0d1e836808c875da34d53ea4204f8
Chase Hilbert <chase@heyspacetime.com>
2019-01-11 22:27:27 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,9 +13,13 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase.upcase == phrase
+  if  phrase == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase.upcase == phrase
+    return "NO, NOT SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
+
+  if
 end
